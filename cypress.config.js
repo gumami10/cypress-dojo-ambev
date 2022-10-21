ad1b338035eb3f5f5ae516bca87d6c3afa724bc9
Eric Groppe Lepore Marques <eric.lepore@ambevtech.com.br>
2022-10-21 15:10:45 -0300

    project in dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'yyw9bk',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -9,3 +10,4 @@ module.exports = defineConfig({
   },
 
 });
+//npx cypress run --record --key af625242-9370-4d3b-99f2-1018c78ed5e9
